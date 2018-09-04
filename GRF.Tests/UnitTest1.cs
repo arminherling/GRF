37e92026eea0078f4c766f801b0286baa325ad5c
Armin Herling <armin.herling@gmail.com>
2018-09-04 21:16:28 +0200

    Added a test grf file

--- GRF.Tests/UnitTest1.cs
+++ GRF.Tests/UnitTest1.cs
@@ -5,10 +5,10 @@ namespace GRF.Tests
     [TestFixture]
     public class UnitTest1
     {
-        [Test]
-        public void Fail()
-        {
-            Assert.Fail();
-        }
+        //[Test]
+        //public void Fail()
+        //{
+        //    Assert.Fail();
+        //}
     }
 }
