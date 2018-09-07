1d54bc8c7b988e72f04533d59ae07883819d674a
Armin Herling <armin.herling@gmail.com>
2018-09-07 23:49:49 +0200

    Testing encoding for travis

--- GRF/GRF.cs
+++ GRF/GRF.cs
@@ -71,7 +71,7 @@ namespace GRF
                 var fileFlags = bodyReader.ReadByte();
                 var fileOffset = bodyReader.ReadInt32();
 
-                Files.Add( fileName );
+                Files.Add( Encoding.Default.GetString( Encoding.Default.GetBytes( fileName ) ) );
             }
 
             IsOpen = true;
