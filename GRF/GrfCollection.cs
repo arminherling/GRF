8409a42d1e4b428944c2942cd929a5e70508d65b
Armin Herling <armin.herling@gmail.com>
2018-09-13 20:50:07 +0200

    Added file for loading collections of grf files

---
+++ GRF/GrfCollection.cs
@@ -0,0 +1,12 @@
+﻿namespace GRF
+{
+    public class GrfCollection
+    {
+        public GrfCollection() { }
+        public GrfCollection( string iniFilePath ) => Load( iniFilePath );
+
+        public void Load( string iniFilePath )
+        {
+        }
+    }
+}
