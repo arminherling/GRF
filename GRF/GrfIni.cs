da548d25efd6b81dceee30f33691739976be01a4
Armin Herling <armin.herling@gmail.com>
2018-09-20 21:54:27 +0200

    Added an ini reader

---
+++ GRF/GrfIni.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace GRF
+{
+    internal class GrfIni
+    {
+        Dictionary<string, Dictionary<string, string>> _sections = new Dictionary<string, Dictionary<string, string>>( StringComparer.InvariantCultureIgnoreCase );
+
+        internal GrfIni() { }
+        internal GrfIni( string path ) => Load( path );
+
+        internal void Load( string path )
+        {
+            if( !File.Exists( path ) )
+                throw new FileNotFoundException( path );
+
+            var currentSection = new Dictionary<string, string>( StringComparer.InvariantCultureIgnoreCase );
+            _sections[""] = currentSection;
+
+            foreach(var line in File.ReadAllLines( path ) )
+            {
+                var trimmedLine = line.Trim();
+
+                if( trimmedLine.StartsWith("[") && trimmedLine.EndsWith( "]" ) )
+                {
+                    var sectionName = trimmedLine.Substring( 1, trimmedLine.Length - 2 );
+
+                    currentSection = new Dictionary<string, string>( StringComparer.InvariantCultureIgnoreCase );
+                    _sections[sectionName] = currentSection;
+                    continue;
+                }
+
+                if( trimmedLine.Contains( "=" ) )
+                {
+                    var split = trimmedLine.Split( '=' );
+                    currentSection.Add( split[0], split[1] );
+                }
+            }
+        }
+
+        internal string GetValue(string section, string key, string defaultValue = "" )
+        {
+            if( !_sections.ContainsKey( section ) )
+                return defaultValue;
+
+            if( !_sections[section].ContainsKey( key ) )
+                return defaultValue;
+
+            return _sections[section][key];
+        }
+
+        internal string[] GetKeys(string section )
+        {
+            if( !_sections.ContainsKey( section ) )
+                return new string[0];
+
+            return _sections[section].Keys.ToArray();
+        }
+    }
+}
