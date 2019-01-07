6d7681015e2b7318a3ded0a185e48bcb745fb95f
Armin Herling <armin.herling@gmail.com>
2019-01-07 18:56:07 +0100

    Fleshed out GrfCollection for loading all files defined in data.ini or similar

--- GRF/GrfCollection.cs
+++ GRF/GrfCollection.cs
@@ -1,12 +1,59 @@
-﻿//namespace GRF
-//{
-//    public class GrfCollection
-//    {
-//        public GrfCollection() { }
-//        public GrfCollection( string iniFilePath ) => Load( iniFilePath );
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
-//        public void Load( string iniFilePath )
-//        {
-//        }
-//    }
-//}
+namespace GRF
+{
+    public class GrfCollection
+    {
+        private List<Grf> _loadedGrfs = new List<Grf>();
+
+        public GrfCollection() { }
+        public GrfCollection( string iniFilePath ) => Load( iniFilePath );
+
+        public void Load( string iniFilePath, string sectionName = "Data" )
+        {
+            _loadedGrfs.Clear();
+
+            var dataIni = new GrfIni( iniFilePath );
+            var directory = Path.GetDirectoryName( iniFilePath );
+            var grfFiles = dataIni.Values( sectionName );
+
+            foreach( var grfFile in grfFiles )
+            {
+                var filePath = Path.Combine( directory, grfFile );
+                _loadedGrfs.Add( new Grf( filePath ) );
+            }
+        }
+
+        public void Unload()
+        {
+            foreach( var grf in _loadedGrfs)
+            {
+                grf.Unload();
+            }
+            _loadedGrfs.Clear();
+        }
+
+        public bool FindEntry( string entryPath, out GrfFile file )
+        {
+            foreach( var grf in _loadedGrfs )
+            {
+                if( grf.Files.TryGetValue( entryPath, out file ) )
+                    return true;
+            }
+            file = null;
+            return false;
+        }
+
+        public List<string> AllFileNames()
+        {
+            var fileNames = new List<string>();
+            foreach( var grf in _loadedGrfs )
+            {
+                fileNames.AddRange( grf.FileNames );
+            }
+            return fileNames.Distinct().ToList();
+        }
+    }
+}
